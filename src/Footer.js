b46f2559572ce902d2a62560da661ff9add41fd1
unknown <jmiccolo89@gmail.com>
2020-12-14 16:35:04 -0500

    Fixing socials

--- src/Footer.js
+++ src/Footer.js
@@ -9,6 +9,11 @@ const Footer = () => {
                 joshmiccolowebdesigns@gmail.com
 
             </address>
+             <nav className="socials">
+        <a href="https://twitter.com/JoshMiccolo" target="_blank" rel="noopener noreferrer" className="fab fa-twitter fa-2x"></a>
+        <a href="https://github.com/Jmiccolo" target="_blank" rel="noopener noreferrer" className="fab fa-github fa-2x"></a>
+        <a href="https://www.linkedin.com/in/josh-miccolo-513b2521/" target="_blank" rel="noopener noreferrer" className="fab fa-linkedin fa-2x"></a>
+      </nav>
         </footer>
     );
 };
