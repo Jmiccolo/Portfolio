932471e748fd348f3fcb8d0f0cd947bbfbaa4fab
unknown <jmiccolo89@gmail.com>
2020-12-18 20:05:51 -0500

    Fixing SVG

--- src/bodyComponents/Landing.js
+++ src/bodyComponents/Landing.js
@@ -3,6 +3,7 @@ import {withRouter, Link} from 'react-router-dom';
 import Umbrella from "../logo/umbrella";
 import Talk1 from "../logo/talk1";
 import Talk2 from "../logo/talk2";
+import Before from "../svgs/before";
 
 const Landing = (props) => {
     return (
@@ -33,20 +34,7 @@ const Landing = (props) => {
                 </svg>
            </section>
            <section className="Performative section">
-                <svg className="section-image" fill="none" viewBox="0 0 3000 2000">
-                <defs>
-                        <style>
-                            @import url("https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
-                        </style>
-                </defs>
-                    <rect width="3000"  height="500" fill="lightgrey" x="0" y="1000" rx="50"/>
-                    <rect id="loadBar" width="0" height="400" fill="green" x="100" y="1050"/>
-                    <Umbrella id="Performance" strokeWidth="35" color={{back:"#afeeff", front:"#5f9eae"}}/>
-                    <text id="X" stroke="red" fill="red" x="2500" y="1450">X</text>
-                    <text id="check" stroke="green" fill="green" x="2500" y="1450">&#x2713;</text>
-                    <text id="before" fontFamily="Work Sans" fill="black" stroke="black" x="1300" y="250">Before</text>
-                    <text id="after" fill="black" fontFamily="Work Sans" stroke="black" x="1300" y="250">After</text>
-                </svg>
+                <Before/>
                 <div className="section-text">
                 <h2>Optimization </h2>
                 <p>I push myself to build fast, well documented and performative websites that get the attention of an audience. I have effectively built backend services with nodejs and have experience in both SQL and NoSQL databases.</p>
